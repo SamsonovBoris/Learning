9f365e49be57eabad5860d6139cbfe3384c3aaf4
SamsonovBoris <samsonov-job@yandex.ru>
2021-03-17 17:07:01 +0300

    Push Lesson01

---
+++ Lessons01/Lessons01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Learning2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как тебя зовут? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}! Сегодня [{DateTime.Now}]");
+        }
+    }
+}

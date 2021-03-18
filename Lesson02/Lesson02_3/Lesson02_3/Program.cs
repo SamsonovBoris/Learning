eeed3f5a0b413d3cb5eb0e359d742f3a6c0b16b4
SamsonovBoris <samsonov-job@yandex.ru>
2021-03-18 23:21:52 +0300

    Push Lesson02_3

---
+++ Lesson02/Lesson02_3/Lesson02_3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Определить, является ли введённое пользователем число чётным.
+
+using System;
+
+namespace Lesson02_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.Write("Введеное число четное");
+            }
+            else
+            {
+                Console.Write("Введеное число нечетное");
+            }
+        }
+    }
+}

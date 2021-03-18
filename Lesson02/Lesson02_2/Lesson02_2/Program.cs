8439a3d597eae74766981e152d38504dd9433c6c
SamsonovBoris <samsonov-job@yandex.ru>
2021-03-18 22:44:19 +0300

    Push Lesson2_02

---
+++ Lesson02/Lesson02_2/Lesson02_2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace les02_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите порядковый номер месяца: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string month = "";
+            switch (number)
+            {
+                case 1:
+                    month = "January";
+                    break;
+                case 2:
+                    month = "February";
+                    break;
+                case 3:
+                    month = "March";
+                    break;
+                case 4:
+                    month = "April";
+                    break;
+                case 5:
+                    month = "May";
+                    break;
+                case 6:
+                    month = "June";
+                    break;
+                case 7:
+                    month = "July";
+                    break;
+                case 8:
+                    month = "August";
+                    break;
+                case 9:
+                    month = "September";
+                    break;
+                case 10:
+                    month = "October";
+                    break;
+                case 11:
+                    month = "November";
+                    break;
+                case 12:
+                    month = "December";
+                    break;
+
+            }
+
+            Console.WriteLine(month);
+
+
+        }
+    }
+}

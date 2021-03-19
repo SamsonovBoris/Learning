02f00f972cbc75ad2c7cc76e1b289ab3d9d30d4b
SamsonovBoris <samsonov-job@yandex.ru>
2021-03-19 21:40:55 +0300

    Push Lesson02_1

---
+++ Lesson02/Lesson02_1/Lesson02_1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Запросить у пользователя минимальную и максимальную температуру за сутки и вывести среднесуточную температуру.
+
+using System;
+
+namespace Lesson02_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите, минимальную температуру за сутки: ");
+            int minTemp = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите, максимаьную температуру за сутки: ");
+            int maxTemp = Convert.ToInt32(Console.ReadLine());
+            int Temp = (minTemp + maxTemp) / 2;
+            Console.WriteLine($"Средняя температура: {Temp}");
+        }
+    }
+}

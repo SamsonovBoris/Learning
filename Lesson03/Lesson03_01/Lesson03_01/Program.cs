4a4a16c00c538dff3838a90afc5b1d9e4dd46b6c
SamsonovBoris <samsonovjovo@gmail.com>
2021-03-22 21:35:15 +0300

    Push Lesson03_01

---
+++ Lesson03/Lesson03_01/Lesson03_01/Program.cs
@@ -0,0 +1,31 @@
+﻿//	Написать программу, выводящую элементы двухмерного массива по диагонали.
+
+using System;
+
+namespace Lesson03_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array2D = new int[7, 7];
+
+            array2D[0, 0] = 1;
+            array2D[1, 1] = 1;
+            array2D[2, 2] = 1;
+            array2D[3, 3] = 1;
+            array2D[4, 4] = 1;
+            array2D[5, 5] = 1;
+            array2D[6, 6] = 1;
+
+            for (int m = 0; m < array2D.GetLength(0); m++)
+            {
+                for (int n = 0; n < array2D.GetLength(1); n++)
+                {
+                    Console.Write($" {array2D[m, n]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

3d479ce454b761e96afe2500ec5a64a01c6ef20b
SamsonovBoris <samsonovjowo@gmail.com>
2021-04-04 22:40:44 +0300

    Push Lesson05_01

---
+++ Lesson05/Lesson05_01/Lesson05_01/Program.cs
@@ -0,0 +1,19 @@
+﻿// Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+
+using System;
+using System.IO;
+
+namespace Lesson05_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите произвольный набор сиволов: ");
+            string symbolString = Console.ReadLine();
+
+            string filename = "/Users/b.samsonov/Documents/test.txt";
+            File.WriteAllText(filename, symbolString); // записываем в файл строку
+        }
+    }
+}
